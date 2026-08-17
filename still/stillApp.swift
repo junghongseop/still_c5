a956432809a2b05accf0ec9f965fa0758512c67e
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-17 21:10:02 +0900

    init

---
+++ still/stillApp.swift
@@ -0,0 +1,17 @@
+//
+//  stillApp.swift
+//  still
+//
+//  Created by 정홍섭 on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct stillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
