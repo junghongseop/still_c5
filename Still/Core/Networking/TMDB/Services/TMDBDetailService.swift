3c9348891b6e50d0a23d5ccc0f2df7a2b22b9742
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-24 10:23:08 +0900

    feat: detail api service 구현

---
+++ Still/Core/Networking/TMDB/Services/TMDBDetailService.swift
@@ -0,0 +1,18 @@
+//
+//  TMDBDetailService.swift
+//  Still
+//
+//  Created by 정홍섭 on 8/21/26.
+//
+
+import Foundation
+
+final class TMDBDetailService {
+    private let client = TMDBClient()
+    
+    func detailMovie(id: Int) async throws -> TMDBMovieDetailResponse{
+       try await client.request(
+            .detailMovie(id: id)
+        )
+    }
+}
