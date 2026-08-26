9af1f1f5047ab779a571ae5cb095fa03f970525b
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-26 15:20:44 +0900

    fix: 컴포넌트 조합 및 홈화면 임시 구현

--- Still/Features/Home/HomeView.swift
+++ Still/Features/Home/HomeView.swift
@@ -8,20 +8,50 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedYear: Int?
+    
+    private let currentYear = DateUtility.currentYear
+    
     var body: some View {
-        Button("검색") {
-            Task {
-                do {
-                    let service = TMDBSearchService()
-                    let movies = try await service.searchMovie(title: "스파이더맨")
-                    
-                    for movie in movies {
-                        Log.debug(movie.id, movie.title)
+        ZStack {
+            StillColors.Surface.base
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("홈")
+                    .foregroundStyle(StillColors.Content.primary)
+                    .bold()
+                    .font(.system(size: 44))
+                
+                HStack(spacing: 8) {
+                    FilterChip(title: "전체", isSelected: selectedYear == nil) {
+                        selectedYear = nil
                     }
-                } catch {
-                    Log.debug(error)
+                    
+                    FilterChip(title: String(currentYear), isSelected: selectedYear == currentYear) {
+                        selectedYear = currentYear
+                    }
                 }
+                
+                MovieSummaryCard(
+                    filter: selectedYear == currentYear ? String(currentYear) :"전체",
+                    content: selectedYear == currentYear
+                        ? "올해는 집에서 더 많이 봤어요"
+                        : "지금까지 극장에서 더 많이 봤어요"
+                )
+                
+                Text("캐릭터")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 406)
+                    .background()
             }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            )
+            .padding(.horizontal, 24)
+            .padding(.vertical, 12)
         }
     }
 }
