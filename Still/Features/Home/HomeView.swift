bdef0c7454ca15a2881b0348d57f4b2a4cd934b9
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-27 10:07:16 +0900

    fix: 모디파이어 적용

--- Still/Features/Home/HomeView.swift
+++ Still/Features/Home/HomeView.swift
@@ -13,46 +13,35 @@ struct HomeView: View {
     private let currentYear = DateUtility.currentYear
     
     var body: some View {
-        ZStack {
-            StillColors.Surface.base
-                .ignoresSafeArea()
+        VStack(alignment: .leading, spacing: 20) {
+            Text("홈")
+                .foregroundStyle(StillColors.Content.primary)
+                .bold()
+                .font(.system(size: 44))
             
-            VStack(alignment: .leading, spacing: 20) {
-                Text("홈")
-                    .foregroundStyle(StillColors.Content.primary)
-                    .bold()
-                    .font(.system(size: 44))
-                
-                HStack(spacing: 8) {
-                    FilterChip(title: "전체", isSelected: selectedYear == nil) {
-                        selectedYear = nil
-                    }
-                    
-                    FilterChip(title: String(currentYear), isSelected: selectedYear == currentYear) {
-                        selectedYear = currentYear
-                    }
+            HStack(spacing: 8) {
+                FilterChip(title: "전체", isSelected: selectedYear == nil) {
+                    selectedYear = nil
                 }
                 
-                MovieSummaryCard(
-                    filter: selectedYear == currentYear ? String(currentYear) :"전체",
-                    content: selectedYear == currentYear
-                        ? "올해는 집에서 더 많이 봤어요"
-                        : "지금까지 극장에서 더 많이 봤어요"
-                )
-                
-                Text("캐릭터")
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 406)
-                    .background()
+                FilterChip(title: String(currentYear), isSelected: selectedYear == currentYear) {
+                    selectedYear = currentYear
+                }
             }
-            .frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity,
-                alignment: .topLeading
+            
+            MovieSummaryCard(
+                filter: selectedYear == currentYear ? String(currentYear) :"전체",
+                content: selectedYear == currentYear
+                ? "올해는 집에서 더 많이 봤어요"
+                : "지금까지 극장에서 더 많이 봤어요"
             )
-            .padding(.horizontal, 24)
-            .padding(.vertical, 12)
+            
+            Text("캐릭터")
+                .frame(maxWidth: .infinity)
+                .frame(height: 406)
+                .background()
         }
+        .screenLayoutStyle()
     }
 }
 
