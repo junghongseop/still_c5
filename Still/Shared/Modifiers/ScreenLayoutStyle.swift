edf0ce8f88347232e06840f608de8bd9cf62450e
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-27 10:07:00 +0900

    feat: 스크린 레이아웃 모디파이어 구현

---
+++ Still/Shared/Modifiers/ScreenLayoutStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenLayoutStyle.swift
+//  Still
+//
+//  Created by 정홍섭 on 8/27/26.
+//
+
+import SwiftUI
+
+struct ScreenLayoutStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScreenLayoutStyle()
+}
