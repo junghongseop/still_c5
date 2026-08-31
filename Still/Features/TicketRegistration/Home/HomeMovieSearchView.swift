78cc81d3017b6e54750b59ff092b9a61ac1003fa
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-08-31 11:41:58 +0900

    fix: 프리뷰 한개 삭제

--- Still/Features/TicketRegistration/Home/HomeMovieSearchView.swift
+++ Still/Features/TicketRegistration/Home/HomeMovieSearchView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct HomeMovieSearchView: View {
     var body: some View {
-        Text("HomeMovieSearchView")
+        ZStack {
+            StillColors.Surface.base
+                .ignoresSafeArea()
+            
+            MovieSearchContent(
+                title: "집에서 본 영화를 찾아요",
+                subTitle: "티켓 없이 영화 제목으로 바로 선택해요"
+            )
+            .padding(.top, 16)
+            .padding(.horizontal, 24)
+            .frame(maxHeight: .infinity, alignment: .topLeading)
+        }
     }
 }
 
